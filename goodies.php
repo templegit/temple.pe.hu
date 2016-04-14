0345cb5f89afbde298ed33a111b44c3b37697b7a
sntch <webmaster@temple.pe.hu>
2016-04-15 00:29:36 +0100

    Added Cyphernomicon

--- goodies.php
+++ goodies.php
@@ -5,6 +5,9 @@
 <?php include('templates/header.php') ?>
 		<p>Welcome to temple.pe.hu! This webserver provides html pages about many cool and interesting topics. Feel free to browse at your leisure and remember... have <b>fun!</b></p>
 		<article>
+			<h3>15/04/2016 &mdash; <a href="https://www.cypherpunks.to/faq/cyphernomicron/cyphernomicon.txt">Cyphernomicon</a></h3>
+			
+		<article>
 			<h3>13/04/2016 &mdash; Cool shapes</h3>
 			<p>Please enjoy this fine collection of shapes. The shapes have been handmade in Microsoft Paint. Each shape roughly took a few seconds to create. The shapes are only available in the black colour however you are free to save the shapes and recolour them as you wish.</p>
 			<img src="goodies/shapes/1.bmp">
