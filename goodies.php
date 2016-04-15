d42ce7e9d3a247603e5db7f0d990b76bb17178fd
vld <webmaster@temple.pe.hu>
2016-04-15 12:59:14 +0100

    Change goodies to retrieve articles dynamically over hard written in goodies.php

--- goodies.php
+++ goodies.php
@@ -1,25 +1,19 @@
 <?php
 	$pageName = "Goodies";
 	$headingName = "Goodies";
+	$latestArticleFileExt = 'html';
+	$latestArticleFileName = '15-04-2016-Cyphernomicon';
+	$latestArticle = $latestArticleFileName.'.'.$latestArticleFileExt;
 ?>
 <?php include('templates/header.php') ?>
 		<p>Welcome to temple.pe.hu! This webserver provides html pages about many cool and interesting topics. Feel free to browse at your leisure and remember... have <b>fun!</b></p>
-		<article>
-			<h3>15/04/2016 &mdash; Cyphernomicon</h3>
-			<p>Click <a href="https://www.cypherpunks.to/faq/cyphernomicron/cyphernomicon.txt">here to read & download the cyphernomicon</a>.</p>
-		</article>
-		<article>
-			<h3>13/04/2016 &mdash; Cool shapes</h3>
-			<p>Please enjoy this fine collection of shapes. The shapes have been handmade in Microsoft Paint. Each shape roughly took a few seconds to create. The shapes are only available in the black colour however you are free to save the shapes and recolour them as you wish.</p>
-			<img src="goodies/shapes/1.bmp">
-			<img src="goodies/shapes/2.bmp">
-			<img src="goodies/shapes/3.bmp">
-			<img src="goodies/shapes/4.bmp">
-			<img src="goodies/shapes/5.bmp">
-			<img src="goodies/shapes/6.bmp">
-			<img src="goodies/shapes/7.bmp">
-			<img src="goodies/shapes/8.bmp">
-			<img src="goodies/shapes/9.bmp">
-			<img src="goodies/shapes/10.bmp">
-		</article>
+		<?php include('goodies/'.$latestArticle) ?>
+		<!-- To do:
+
+			Add the rest of the articles as a links.
+			This may possible require a file system read or 
+			if permissions do not allow for that on the host 
+			webserver maybe a manifest.
+
+		-->
 <?php include('templates/footer.php') ?>
