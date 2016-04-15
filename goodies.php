c9f1958fa84bb66df5a65b104bb72b38c9998e8c
sntch <webmaster@temple.pe.hu>
2016-04-15 16:49:05 +0100

    Changed latest article to vocaroos

--- goodies.php
+++ goodies.php
@@ -1,5 +1,5 @@
 <?php
-	$latestArticleFileName = '15-04-2016-Pupinia';
+	$latestArticleFileName = '15-04-2016-Vocaroos';
 	$latestArticle = $latestArticleFileName.'.php';
 ?>
 <?php include('goodies/'.$latestArticle) ?>
