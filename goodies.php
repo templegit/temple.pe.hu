1e06678def11e417396a8dd135b6b117775bfb37
vld <webmaster@temple.pe.hu>
2016-04-15 13:46:50 +0100

    Remove todo

--- goodies.php
+++ goodies.php
@@ -28,14 +28,6 @@
 ?>
 <?php include('templates/header.php') ?>
 		<?php include('goodies/'.$latestArticle) ?>
-		<!-- To do:
-
-			Add the rest of the articles as a links.
-			This may possible require a file system read or 
-			if permissions do not allow for that on the host 
-			webserver maybe a manifest.
-
-		-->
 		<ul id="articles">
 		<?php 
 			foreach ($fileNames as $file) {
