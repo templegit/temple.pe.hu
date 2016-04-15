e091cd478e2b06d7d0aa30588f09e6aac330e270
vld <webmaster@temple.pe.hu>
2016-04-15 13:02:19 +0100

    Remove reduntant text

--- goodies.php
+++ goodies.php
@@ -6,7 +6,6 @@
 	$latestArticle = $latestArticleFileName.'.'.$latestArticleFileExt;
 ?>
 <?php include('templates/header.php') ?>
-		<p>Welcome to temple.pe.hu! This webserver provides html pages about many cool and interesting topics. Feel free to browse at your leisure and remember... have <b>fun!</b></p>
 		<?php include('goodies/'.$latestArticle) ?>
 		<!-- To do:
 
