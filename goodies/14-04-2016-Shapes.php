ec19bcff755362ffd428c1d336aadabf407118e7
vld <webmaster@temple.pe.hu>
2016-04-15 14:15:40 +0100

    Set shape image src absolute

--- goodies/14-04-2016-Shapes.php
+++ goodies/14-04-2016-Shapes.php
@@ -31,15 +31,15 @@
 		<article>
 			<h3>13/04/2016 &mdash; Cool shapes</h3>
 			<p>Please enjoy this fine collection of shapes. The shapes have been handmade in Microsoft Paint. Each shape roughly took a few seconds to create. The shapes are only available in the black colour however you are free to save the shapes and recolour them as you wish.</p>
-			<img src="goodies/shapes/1.bmp">
-			<img src="goodies/shapes/2.bmp">
-			<img src="goodies/shapes/3.bmp">
-			<img src="goodies/shapes/4.bmp">
-			<img src="goodies/shapes/5.bmp">
-			<img src="goodies/shapes/6.bmp">
-			<img src="goodies/shapes/7.bmp">
-			<img src="goodies/shapes/8.bmp">
-			<img src="goodies/shapes/9.bmp">
-			<img src="goodies/shapes/10.bmp">
+			<img src="/goodies/shapes/1.bmp">
+			<img src="/goodies/shapes/2.bmp">
+			<img src="/goodies/shapes/3.bmp">
+			<img src="/goodies/shapes/4.bmp">
+			<img src="/goodies/shapes/5.bmp">
+			<img src="/goodies/shapes/6.bmp">
+			<img src="/goodies/shapes/7.bmp">
+			<img src="/goodies/shapes/8.bmp">
+			<img src="/goodies/shapes/9.bmp">
+			<img src="/goodies/shapes/10.bmp">
 		</article>
 <?php include('../templates/footer.php') ?>
