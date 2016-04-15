8e7e8dfa199c9f4c04a2fd87ad21bf62225a9c55
vld <webmaster@temple.pe.hu>
2016-04-15 13:19:58 +0100

    Add initial file read for goodie articles dir

--- goodies.php
+++ goodies.php
@@ -1,6 +1,24 @@
 <?php
 	$pageName = "Goodies";
 	$headingName = "Goodies";
+
+	// read all articles
+
+	$directory = "goodies/";
+	$fileNames = array();
+
+	// Open a directory, and read its contents
+	if (is_dir($dir)){
+		if ($dh = opendir($dir)){
+		    while (($file = readdir($dh)) !== false){
+		      	array_push($file);
+		    }
+		    closedir($dh);
+	  	}
+	}
+
+	// latest file is
+
 	$latestArticleFileExt = 'html';
 	$latestArticleFileName = '15-04-2016-Cyphernomicon';
 	$latestArticle = $latestArticleFileName.'.'.$latestArticleFileExt;
@@ -15,4 +33,9 @@
 			webserver maybe a manifest.
 
 		-->
+		<?php 
+		foreach ($fileNames as $file) {
+		    echo "<br>".$file;
+		}
+		?>
 <?php include('templates/footer.php') ?>
