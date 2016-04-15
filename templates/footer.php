dcbfd93a55a4cd4a31941b3e9f9349783a32525a
vld <webmaster@temple.pe.hu>
2016-04-15 14:13:48 +0100

    Set absolute resources

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 	</div>
-	<audio autoplay src="audio/sfx/close.wav"></audio>
+	<audio autoplay src="/audio/sfx/close.wav"></audio>
 	<p id="footer">Slysheng and Rondo &copy; 2016</p>
 </body>
 
--- templates/header.php
+++ templates/header.php
@@ -3,19 +3,19 @@
 <head>
 	<title>temple.pe.hu - <?php echo $pageName ?></title>
 	<meta charset="utf8">
-	<link rel="stylesheet" href="style.css">
-	<script src="js/script.js"></script>
+	<link rel="stylesheet" href="/style.css">
+	<script src="/js/script.js"></script>
 </head>
 <body>
-	<h1><img src="temple.gif"><br>temple.pe.hu</h1>
+	<h1><img src="/temple.gif"><br>temple.pe.hu</h1>
 	<ul id="navigation">
 		<li><a href="/">Home</a></li>
-		<li><a href="about.php">About</a></li>
-		<li><a href="stories.php">Stories</a></li>
-		<li><a href="goodies.php">Goodies</a></li>
-		<li><a href="contact.php">Contact</a></li>
-		<li><a href="punish.html">Be punished</a></li>
+		<li><a href="/about.php">About</a></li>
+		<li><a href="/stories.php">Stories</a></li>
+		<li><a href="/goodies.php">Goodies</a></li>
+		<li><a href="/contact.php">Contact</a></li>
+		<li><a href="/punish.html">Be punished</a></li>
 	</ul>
-	<audio id="navigationsfx" src="audio/sfx/open.wav"></audio>
+	<audio id="navigationsfx" src="/audio/sfx/open.wav"></audio>
 	<div id="content">
 	<h2><?php echo $headingName?></h2>
