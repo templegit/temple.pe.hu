ab680409015c6f851114e34e27f7f5c13f1372ac
vld <webmaster@temple.pe.hu>
2016-04-28 12:35:24 +0100

    Add good image

--- contact.php
+++ contact.php
@@ -4,4 +4,5 @@
 ?>
 <?php include('templates/header.php') ?>
 		<p>Contact the web master at <a href="mailto:webmaster@temple.pe.hu">webmaster@temple.pe.hu</a>.</p>
+		<p><img src=/images/copkilla.jpg title="high energy avian"></p>
 <?php include('templates/footer.php') ?>
